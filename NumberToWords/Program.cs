20c2d741cedadb5afd35d2314c3335c45c0a2bf8
Saturnino <isc_saturnino@outlook.com>
2015-05-27 19:47:35 -0500

    First commit

---
+++ NumberToWords/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.International;
+using Microsoft.International.Formatters;
+
+namespace NumberToWords
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            int i = 10;
+
+            Microsoft.International.FormatCultureInfo cultureInfo = new FormatCultureInfo("es-ES");
+
+            Console.WriteLine(string.Format(cultureInfo,"{0:L}",i));
+            Console.ReadLine();
+        }
+
+    }
+}
